e1ba5178b34ff550c5a3ed1e95be80aeccc7f7d5
Arpit-Babbar <arpitbabbar@gmail.com>
2024-01-19 20:31:05 +0530

    Update KH

--- kh/run_kh_schaal.jl
+++ kh/run_kh_schaal.jl
@@ -20,13 +20,13 @@ numerical_flux = Eq.rusanov
 
 bound_limit = "yes"
 bflux = evaluate
-final_time = 0.4
+final_time = 1.0
 
 nx, ny = 512, 512
 cfl = 0.0
 bounds = ([-Inf],[Inf]) # Not used in Euler
 save_iter_interval = 0
-save_time_interval = final_time / 100.0
+save_time_interval = final_time / 10.0
 animate = true # Factor on save_iter_interval or save_time_interval
 compute_error_interval = 0
 
@@ -44,14 +44,15 @@ limiter = setup_limiter_blend(
                               indicating_variables = Eq.rho_p_indicator!,
                               reconstruction_variables = conservative_reconstruction,
                               indicator_model = "gassner",
-                              debug_blend = true
+                              debug_blend = false
                              )
 scheme = Scheme(solver, degree, solution_points, correction_function,
                 numerical_flux, bound_limit, limiter, bflux)
 param = Parameters(grid_size, cfl, bounds, save_iter_interval,
                    save_time_interval, compute_error_interval,
                    animate = animate,
-                   cfl_safety_factor = cfl_safety_factor)
+                   cfl_safety_factor = cfl_safety_factor,
+   		   saveto = joinpath(@__DIR__, "output") )
 #------------------------------------------------------------------------------
 sol = Tenkai.solve(equation, problem, scheme, param);
 
